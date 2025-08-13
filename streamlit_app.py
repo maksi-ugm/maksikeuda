aff11885fbfecbbc01f7d26b209c702d61b06a6e
Magister Akuntansi UGM <nugi.xmode@gmail.com>
2025-08-14 06:21:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -238,6 +238,7 @@ if logo_base64:
     <div class="custom-header">
         <img src="data:image/jpeg;base64,{logo_base64}" width="100">
         <div>
+            <h4>Pogram Studi Magister Akuntansi</h4>
             <h2>Fakultas Ekonomika dan Bisnis</h2>
             <h4>Universitas Gadjah Mada</h4>
         </div>
